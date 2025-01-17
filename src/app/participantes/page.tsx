a707d70c787ca42ea6ef635eb918811e37046ea7
Jhonatan Sarmento <jhonatan.sarmento@gmail.com>
2025-01-17 16:53:09 -0400

    feat: add participants page with basic structure

--- src/app/participantes/page.tsx
+++ src/app/participantes/page.tsx
@@ -1,45 +1,19 @@
-'use client';
+import { db } from "@/lib/prisma";
 
-import { useEffect, useState } from 'react';
-
-export default function ParticipantsPage() {
-  const [participants, setParticipants] = useState([]);
-
-  useEffect(() => {
-    async function fetchParticipants() {
-      try {
-        const response = await fetch('/api/participants');
-        if (!response.ok) throw new Error('Erro ao buscar participantes');
-        const data = await response.json();
-        setParticipants(data);
-      } catch (error) {
-        console.error('Erro ao buscar participantes:', error);
-      }
-    }
-
-    fetchParticipants();
-  }, []);
+export default async function ParticipantsPage() {
+  const participants = await db.participant.findMany();
 
   return (
     <div>
-      <div className='p-6 space-y-6'>
+      <div className="space-y-6 p-6">
         {/* TÍTULO E BOTÃO */}
-        <div className='flex h-full items-center justify-between'>
-          <h1 className='text-2xl font-bold'>Cadastro de Participantes</h1>
-        </div>
-
-        {/* LISTAGEM DOS PARTICIPANTES */}
-        <div className='space-y-2'>
-          {participants.length > 0 ? (
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            participants.map((participant: any) => (
-              <div key={participant.id} className='p-2 bg-gray-100 rounded'>
-                {participant.firstName} - {participant.phone}
-              </div>
-            ))
-          ) : (
-            <p>Nenhum participante encontrado.</p>
-          )}
+        <div className="flex h-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Cadastro de Participantes</h1>
+          {participants.map((participant) => (
+            <div key={participant.id}>
+              {participant.firstName} - {participant.phone}
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedprisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedprisma) {
+    global.cachedprisma = new PrismaClient();
+  }
+  prisma = global.cachedprisma;
+}
 
 export const db = prisma;
