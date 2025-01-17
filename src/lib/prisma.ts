9c58ae2c668924b7743e75c2509873bc3df9f7bc
Jhonatan Sarmento <jhonatan.sarmento@gmail.com>
2025-01-17 15:50:11 -0400

    fix: simplify Prisma client initialization for production compatibility
    - Removed global caching logic in Prisma setup.
    - Ensured direct initialization of PrismaClient for better compatibility in production environments.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,5 @@
-/* eslint-disable no-unused-vars */
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var cachedprisma: PrismaClient;
-}
-
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedprisma) {
-    global.cachedprisma = new PrismaClient();
-  }
-  prisma = global.cachedprisma;
-}
+const prisma = new PrismaClient();
 
 export const db = prisma;
