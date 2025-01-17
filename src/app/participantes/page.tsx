b6535afc6390d50569ad119c6dd066bdae4d01d8
Jhonatan Sarmento <jhonatan.sarmento@gmail.com>
2025-01-17 15:43:33 -0400

    feat: add participants page with basic structure
    - Added a participants page with a list of participants fetched from the database.
    - Displaying participant names and phone numbers as a simple list.

---
+++ src/app/participantes/page.tsx
@@ -0,0 +1,21 @@
+import { db } from '@/lib/prisma';
+
+export default async function ParticipantsPage() {
+  const participants = await db.participant.findMany();
+
+  return (
+    <div>
+      <div className='p-6 space-y-6'>
+        {/* TÍTULO E BOTÃO */}
+        <div className='flex h-full items-center justify-between'>
+          <h1 className='text-2xl font-bold'>Cadastro de Participantes</h1>
+          {participants.map((participant) => (
+            <div key={participant.id}>
+              {participant.firstName} - {participant.phone}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedprisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedprisma) {
+    global.cachedprisma = new PrismaClient();
+  }
+  prisma = global.cachedprisma;
+}
+
+export const db = prisma;
